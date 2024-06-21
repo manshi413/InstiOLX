cde45d18575b2425d647c74c2c0dc5b9c1578599
Manshi Yadav <manshiyadav413@gmail.com>
2024-06-22 01:00:03 +0530

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.querySelector(".webName").addEventListener("click",()=>{
     window.open(window.location.href,"_blank");
 });
+document.querySelector(".button").addEventListener("click",()=>{
+    console.log("Search has been completed");
+})
